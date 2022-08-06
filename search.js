eadcb2d88a430351c763e4d09ded862c9c85801d
bd-dev-ismail <ismailratul7@gmail.com>
2022-08-06 22:22:21 +0600

    includes indexof startwiths endwiths

--- search.js
+++ search.js
@@ -6,4 +6,20 @@ const searchString = 'Athi'
 // const searchStringLower = searchString.toLowerCase();
 // const doesExist = lower.includes(searchStringLower);
 const doesExist = lyrics.toLowerCase().includes(searchString.toLowerCase());
-console.log(doesExist);
+// console.log(doesExist);
+
+console.log(lyrics.indexOf('jab'))
+console.log(lyrics.indexOf('Tu'));
+if(lyrics.indexOf('hu') !== -1){
+    console.log('Exist inside the String');
+}
+else{
+    console.log('Cannot Find It');
+}
+//Start With
+console.log(lyrics.startsWith('Tu'));
+
+//endswtih
+const fileName = 'mybiodata.pdf';
+const otherFile = 'mypic.png';
+console.log(fileName.endsWith('pdf'));
