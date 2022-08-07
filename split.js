a4937dbfc6abeb1e7cb412be7eb380f2142a95d9
bd-dev-ismail <ismailratul7@gmail.com>
2022-08-07 23:19:07 +0600

    string = split, slice , concat , partial

---
+++ split.js
@@ -0,0 +1,18 @@
+const lyrics = 'Tu athi hai chine mai..jab jab sache barte hu';
+const parts = lyrics.split(' ');
+const sentenes = lyrics.split('.');
+const char = lyrics.split('');
+
+
+// console.log(char);
+const divide = lyrics.slice(5, 10);
+// console.log(divide);
+const partial2 = lyrics.substring(5, 10);
+// console.log(partial2);
+const str1 = 'Ratul';
+const str2 = 'Nowrin!';
+// console.log(str1.concat('+' ,str2));
+
+const lines = ['Tu athi hai chine mai .. jav jav sache varta hu ..'];
+const line2 = lines.join('----');
+console.log(line2);
